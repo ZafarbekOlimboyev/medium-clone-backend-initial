ea803cbf7b0b7c7c8f829c8e3d07bdaa6fe0d1f0
Your Name <zafarbekolimboyev07@gmail.com>
2024-09-18 17:21:22 +0500

    custom user created

--- core/settings.py
+++ core/settings.py
@@ -18,13 +18,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
+    # "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'rest_framework',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -121,3 +122,5 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
 }
+
+AUTH_USER_MODEL = 'users.CustomUser'
