116dec496880bb4fcd7e3cbe673b8ad98f06bd9a
Your Name <zafarbekolimboyev07@gmail.com>
2024-09-27 17:26:26 +0500

    1

--- users/views.py
+++ users/views.py
@@ -1,4 +1,4 @@
-from rest_framework import status, permissions, generics
+from rest_framework import status, permissions, generics, parsers
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import authenticate
